ae1d80db6ede62359745caaaebfa700fddf68434
Julac Ontina <eneioarzew@gmail.com>
2021-05-01 16:28:33 +0800

    API creation using the framework is now enabled.

--- index.php
+++ index.php
@@ -3,18 +3,19 @@
 // Turn line below into a comment to enable PHP error reporting.
 error_reporting(0);
 
-include_once 'ChromePhp.php';
-
 include_once 'config.php';
 include_once 'routes.php';
 
 $original_URI = $_SERVER['REQUEST_URI'];
 $exploded_URI = explode("/", $original_URI);
 $json_string = file_get_contents("php://input");
+$URI_offset = 2;
 
 if ($ENVIRONMENT === "LOCAL") {
-	$temp = array_shift($exploded_URI);
-	$temp = array_shift($exploded_URI);
+	foreach (range(0, $URI_offset - 1) as $i) {
+		array_shift($exploded_URI);
+	}
+
 	$final_URI = implode("/", $exploded_URI);
 	$URI_length = 0;
 
@@ -32,6 +33,7 @@ if ($ENVIRONMENT === "LOCAL") {
 	} else {
 		$controller = $exploded_URI[0];
 		$function = $exploded_URI[1];
+		$data = $exploded_URI[2];
 
 		$controller[0] = strtoupper($controller[0]);
 		$controller .= "Controller";
@@ -40,21 +42,13 @@ if ($ENVIRONMENT === "LOCAL") {
 
 		$obj = json_decode($json_string, true);
 
-		if (empty($obj)) {
-			if (isset($ROUTE[$final_URI])) {
-				$response = $instance->$function();
+		if (isset($ROUTE[$final_URI])) {
+			if ($obj) {
+				$response = $instance->$function($obj);
+				echo json_encode($response);
 			} else {
-				showErrorPage();
+				$response = $instance->$function($data);
 			}
-		} else {
-			$request_function = $obj['function'];
-			$id = $exploded_URI[1];
-			$data = array(
-				"id" => $id,
-				"obj" => $obj,
-			);
-			$response = $instance->$request_function($data);
-			echo json_encode($response);
 		}
 
 		if ($response !== null) {
@@ -62,7 +56,14 @@ if ($ENVIRONMENT === "LOCAL") {
 		}
 	}
 } else if ($ENVIRONMENT === "PRODUCTION") {
-	// enter code here
+	$URI_offset = 1;
+
+	foreach (range(0, $URI_offset - 1) as $i) {
+		array_shift($exploded_URI);
+	}
+
+	$final_URI = implode("/", $exploded_URI);
+	$URI_length = 0;
 }
 
 function showErrorPage()
